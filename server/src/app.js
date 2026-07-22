4b74c6b4bbfadf6f969c7b4620b9533336dd4b71
bhanuprakash1312 <bp136897@gmail.com>
2026-07-22 17:47:17 +0530

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json({
+        success:true,
+        message:"Welcome to Mini ERP CRM"
+    });
+});
+
+module.exports = app;
